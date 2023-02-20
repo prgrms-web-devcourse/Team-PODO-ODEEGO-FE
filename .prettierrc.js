412f137fd2a45c23f8133743ece31b0127db74e4
Seung-hwan285 <ghks123285@gmail.com>
2023-02-20 15:14:31 +0900

    chore: husky,lint-staged,prettier,eslint

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  printWidth: 80,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: "auto",
+  jsxSingleQuote: true,
+  bracketSameLine: true,
+  quoteProps: "as-needed",
+  trailingComma: "es5",
+  arrowParens: "always",
+  bracketSpacing: true,
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+// [ ] : husky 설치
+// [ ] : lint-staged 설치
+// [ ] : emotion
+// [ ] : recoil
+// [ ] : react-query
+// [ ] : cypress
+// [ ] : mui
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
