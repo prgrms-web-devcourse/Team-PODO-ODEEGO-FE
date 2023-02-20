7b413be36e2257fb5ab8be963ddcd2c3db7c08ed
Seung-hwan285 <ghks123285@gmail.com>
2023-02-20 17:09:33 +0900

    chore : recoil,react-query,mui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,28 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import React from "react";
+import { RecoilRoot } from "recoil";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // [x] : husky 설치
 // [x] : lint-staged 설치
-// [x] : emotion
-// [ ] : recoil
-// [ ] : react-query
+// [x] : styled
+// [x] : recoil
+// [x] : react-query
+// [x] : mui
 // [ ] : cypress
-// [ ] : mui
+
+////////////////////////////////////////////////////////////
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  // 서로 다른 사용자 요청 사이 데이터 공유 안되게
+  const [queryClient] = React.useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <Component {...pageProps} />;
+      </RecoilRoot>
+    </QueryClientProvider>
+  );
 }
