8816294451f30ae1e9961487707de37b7ee2d2e1
Kal-MH <rkfalgus713@gmail.com>
2023-02-22 08:50:39 +0900

    refactor: use-multiple-inputs 훅 리팩토링 - 불필요한 함수 제거 및 간소화

--- src/hooks/use-multiple-inputs.ts
+++ src/hooks/use-multiple-inputs.ts
@@ -1,6 +1,4 @@
 import { searchState } from "@/recoil/search-state";
-import { searchProps } from "@/types/search-props";
-
 import { MouseEvent, useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 
@@ -9,64 +7,28 @@ interface InputItem {
 }
 
 const useMultipleInputs = () => {
-  const [inputs, setInputs] = useState<InputItem[]>([]);
+  const [inputs, setInputs] = useState<InputItem[]>([
+    {
+      roadAddress: "",
+    },
+    {
+      roadAddress: "",
+    },
+  ]);
   const [addressList, setAddressList] = useRecoilState(searchState);
 
   useEffect(() => {
-    const inputList = [];
+    const newInputs = addressList.map((a) => ({ roadAddress: a.address }));
+    const emptyInputs = new Array(2 - newInputs.length).fill({
+      roadAddress: "",
+    });
 
-    for (const a of addressList) {
-      const newAddress = {
-        roadAddress: a.address,
-      };
-
-      inputList.push(newAddress);
-    }
-
-    for (let i = inputList.length; i < 2; i++) {
-      inputList.push({
-        roadAddress: "",
-      });
-    }
-
-    setInputs(inputList);
+    setInputs([...newInputs, ...emptyInputs]);
   }, [addressList]);
 
   const addInput = (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-
-    const newInput = {
-      roadAddress: "",
-    };
-
-    setInputs((prev) => [...prev, newInput]);
-  };
-
-  const removeInputElement = (
-    inputList: InputItem[],
-    addressList: searchProps[],
-    index: number
-  ) => {
-    inputList.splice(index, 1);
-    setInputs(inputList);
-
-    addressList.splice(index, 1);
-    setAddressList(addressList);
-  };
-
-  const removeInputAndInsertElement = (
-    inputList: InputItem[],
-    addressList: searchProps[],
-    index: number
-  ) => {
-    const deletedInput = { roadAddress: "" };
-    const deletedAddress = { ...addressList[index], address: "" };
-
-    inputList.splice(index, 1, deletedInput);
-    setInputs(inputList);
-
-    addressList.splice(index, 1, deletedAddress);
-    setAddressList(addressList);
+    setInputs((prev) => [...prev, { roadAddress: "" }]);
   };
 
   const removeInput = (e: MouseEvent<HTMLButtonElement>, index: number) => {
@@ -74,22 +36,18 @@ const useMultipleInputs = () => {
 
     if (index === 0 || index === 1) {
       if (!inputs[index] || inputs[index].roadAddress === "") return;
-    }
-
-    const inputsCopy = JSON.parse(JSON.stringify(inputs));
-    const addressListCopy = JSON.parse(JSON.stringify(addressList));
-
-    if (index === 0 || index === 1) {
-      removeInputAndInsertElement(inputsCopy, addressListCopy, index);
+      setAddressList((prev) =>
+        prev.map((address, i) =>
+          i === index ? { ...address, address: "" } : address
+        )
+      );
+      setInputs((prev) =>
+        prev.map((input, i) => (i === index ? { roadAddress: "" } : input))
+      );
     } else {
-      removeInputElement(inputsCopy, addressListCopy, index);
+      setAddressList((prev) => prev.filter((_, i) => i != index));
+      setInputs((prev) => prev.filter((_, i) => i !== index));
     }
-
-    // inputsCopy.splice(index, 1);
-    // setInputs(inputsCopy);
-
-    // addressListCopy.splice(index, 1);
-    // setAddressList(addressListCopy);
   };
 
   return { inputs, addInput, removeInput };
