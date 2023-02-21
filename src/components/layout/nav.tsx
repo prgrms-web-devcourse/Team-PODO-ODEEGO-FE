65000ad18e1895a54d7261f391bfdb77c1dd6b35
Kal-MH <rkfalgus713@gmail.com>
2023-02-21 12:00:13 +0900

    rename: 이름이 같아서 누락됐던 부분, 다른 이름으로 수정

--- src/components/layout/nav.tsx
+++ src/components/layout/nav.tsx
@@ -1,13 +1,21 @@
+import { COLORS } from "@/constants/css";
+import { ColorProps } from "@/types/css-props";
 import styled from "@emotion/styled";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 const Nav = () => {
-  return <NavContainer>Nav</NavContainer>;
+  return (
+    <NavContainer colors={COLORS}>
+      <AccountCircleIcon />
+    </NavContainer>
+  );
 };
 
-const NavContainer = styled.nav`
+const NavContainer = styled.nav<{ colors: ColorProps }>`
   height: 35px;
   padding: 5px 10px;
-  background-color: beige;
+  color: white;
+  background-color: ${({ colors }) => colors.backgroundAccent};
 `;
 
 export default Nav;
