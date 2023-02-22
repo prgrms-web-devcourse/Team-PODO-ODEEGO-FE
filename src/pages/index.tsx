b0b20b84ad6c9bc8546ecca6c4bbcec63d0ab821
Seung-hwan285 <ghks123285@gmail.com>
2023-02-22 20:23:06 +0900

    refactor : 예비 라우팅 제거

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,11 @@
 import Header from "@/components/home/home-header";
 import Main from "@/components/home/home-main";
-import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Main />
-
-      <div>
-        <Link href='/search'>search</Link>
-      </div>
-      <div>
-        <Link href='/place'>place</Link>
-      </div>
-
-      <div>
-        <Link href='/category'>blog</Link>
-      </div>
     </>
   );
 }
