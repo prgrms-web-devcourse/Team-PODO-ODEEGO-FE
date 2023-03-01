89e03afc8eac1cc4c30909ce83fe40bc1aac4947
JaeKwan Ahn <52008@naver.com>
2023-03-01 20:17:04 +0900

    test: 프리뷰에서 카카오맵 key 테스트해보기

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { RecoilRoot } from "recoil";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Layout from "../components/layout/layout";
-import Script from "next/script";
+// import Script from "next/script";
 import { ThemeProvider } from "@emotion/react";
 import { theme } from "@/styles/theme";
 
@@ -17,11 +17,11 @@ declare global {
 export default function App({ Component, pageProps }: AppProps) {
   // 서로 다른 사용자 요청 사이 데이터 공유 안되게
   const [queryClient] = React.useState(() => new QueryClient());
-  function kakaoInit() {
-    // 페이지가 로드되면 실행
-    window.Kakao.init(process.env.NEXT_PUBLIC_LOGIN);
-    console.log(window.Kakao.isInitialized());
-  }
+  // function kakaoInit() {
+  //   // 페이지가 로드되면 실행
+  //   window.Kakao.init(process.env.NEXT_PUBLIC_LOGIN);
+  //   console.log(window.Kakao.isInitialized());
+  // }
 
   return (
     <>
@@ -31,9 +31,9 @@ export default function App({ Component, pageProps }: AppProps) {
             {globalStyles}
             <Layout>
               <Component {...pageProps} />
-              <Script
+              {/* <Script
                 src='https://developers.kakao.com/sdk/js/kakao.js'
-                onLoad={kakaoInit}></Script>
+                onLoad={kakaoInit}></Script> */}
             </Layout>
           </ThemeProvider>
         </RecoilRoot>
