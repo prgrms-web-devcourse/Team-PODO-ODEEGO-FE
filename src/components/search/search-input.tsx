3d2b1ff9a73346e5a41ff45c0fd4d2a83c487f29
JaeKwan Ahn <52008@naver.com>
2023-03-03 17:07:34 +0900

    Update search-input.tsx

--- src/components/search/search-input.tsx
+++ src/components/search/search-input.tsx
@@ -25,10 +25,10 @@ const SearchInput = () => {
 
   const router = useRouter();
 
-  window.addEventListener("popstate", () => {
-    console.log("goback");
-    closeModal();
-  });
+//   window.addEventListener("popstate", () => {
+//     console.log("goback");
+//     closeModal();
+//   });
 
   const setLoginModalContent = () => {
     return <p>로그인 되어 있지 않아 로그인 페이지로 이동합니다</p>;
