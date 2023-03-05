2f2f94f1ccb80ebcd438bbc087c07ae8378aa194
Seung-hwan285 <ghks123285@gmail.com>
2023-03-05 23:41:30 +0900

    refactor : storage type 추가

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,35 +1,36 @@
+type LocalStorage = typeof window.localStorage;
+
 export const getLocalStorage = (key: string, defaultValue = "") => {
   if (defaultValue === undefined) return;
 
-  const storage = localStorage;
+  const storage: LocalStorage = localStorage;
   try {
     const storedValue = JSON.parse(storage.getItem(key) || "");
 
     return storedValue;
   } catch (error) {
-    console.error(error);
-    return defaultValue;
+    throw new Error((<Error>error).message);
   }
 };
 
 export const setLocalStorage = <T>(key: string, value: T) => {
   if (value === undefined) return;
 
-  const storage = localStorage;
+  const storage: LocalStorage = localStorage;
   try {
     storage.setItem(key, JSON.stringify(value));
   } catch (error) {
-    console.error(error);
+    throw new Error((<Error>error).message);
   }
 };
 
 export const removeLocalStorage = (key: string) => {
   if (key === "") return;
 
-  const storage = localStorage;
+  const storage: LocalStorage = localStorage;
   try {
     storage.removeItem(key);
   } catch (error) {
-    console.error(error);
+    throw new Error((<Error>error).message);
   }
 };
