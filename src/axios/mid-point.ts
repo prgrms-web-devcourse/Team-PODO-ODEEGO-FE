1566feecf2f799dd50b7f718968cd187893914a3
Jane Ro <janero95@gmail.com>
2023-03-07 17:31:29 +0900

    refactor: filteredAddressList 제거

--- src/axios/mid-point.ts
+++ src/axios/mid-point.ts
@@ -4,21 +4,11 @@ import HTTP from "./config/axios-instance";
 
 export const MidPointApi = {
   postMidPoint: async (addressList: searchProps[]) => {
-    const filteredAddressList = addressList
-      .filter((a) => a.address !== "")
-      .map((a) => {
-        return {
-          stationName: a.stationName.split(" ")[0],
-          lat: parseFloat(a.lat),
-          lng: parseFloat(a.lng),
-        };
-      });
-
     try {
       const { data } = await HTTP.post({
         url: "/v1/mid-point/search",
         data: {
-          stations: filteredAddressList,
+          stations: addressList,
         },
       });
 
