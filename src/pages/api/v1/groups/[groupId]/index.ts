5cbe31c684aa385f9f89f5e8d958a4dcc6b822df
Jane Ro <janero95@gmail.com>
2023-03-07 17:29:37 +0900

    feat: 모임 get, delete API 모듈

---
+++ src/pages/api/v1/groups/[groupId]/index.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  //TODO
+  // - groups api로 바꿀 것
+  const { groupId } = req.query;
+  const requestUrl = `${process.env.NEXT_PUBLIC_API_END_POINT}/api/test/groups/${groupId}`;
+
+  try {
+    const { data } = await axios({
+      url: requestUrl,
+      method: "get",
+    });
+
+    res.status(200).json(data);
+  } catch (e) {
+    console.error(e);
+    res.status(400).json({ error: "Error", status: 400 });
+  }
+}
