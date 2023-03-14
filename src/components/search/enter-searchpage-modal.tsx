b1105989f8c396d546b79b5d4763aff2b1f21395
Jane Ro <janero95@gmail.com>
2023-03-15 01:20:39 +0900

    refactor: h1 태그 제거

--- src/components/search/enter-searchpage-modal.tsx
+++ src/components/search/enter-searchpage-modal.tsx
@@ -2,22 +2,14 @@ import styled from "@emotion/styled";
 
 export const EnterSearchPageModal = () => {
   return (
-    <>
-      {/* 닉네임을 받아올 수 있는가? */}
-      <H1>000님이 주소를 요청했습니다.</H1>
-      <Wrapper>
-        <Span>약속 장소를 찾기 위해 주소가 필요합니다.</Span>
-        <Span>계속 하시겠습니까?</Span>
-      </Wrapper>
-    </>
+    <Wrapper>
+      <Span>약속 장소를 찾기 위해 주소가 필요합니다.</Span>
+      <Span>계속 하시겠습니까?</Span>
+    </Wrapper>
   );
 };
 
 export default EnterSearchPageModal;
-
-const H1 = styled.h1`
-  font-size: 2rem;
-`;
 
 const Span = styled.span`
   margin: 0;
