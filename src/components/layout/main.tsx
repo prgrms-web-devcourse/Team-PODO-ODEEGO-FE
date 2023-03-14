bc96a75385fd66cc0ac4ab158c631da804027056
Jane Ro <janero95@gmail.com>
2023-03-14 22:58:51 +0900

    refactor: 전체 레이아웃에 박스 쉐도우 추가

--- src/components/layout/main.tsx
+++ src/components/layout/main.tsx
@@ -28,7 +28,6 @@ const MainContainer = styled.main`
   display: flex;
   flex-direction: column;
   align-items: center;
-  box-shadow: -2px 0 4px -5px #333, 2px 0 4px -5px #333;
   user-select: none;
   padding: 0 1.2rem;
   box-sizing: border-box;
